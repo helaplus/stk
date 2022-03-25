e5f57c25a1dced01ffeb0e8585ea21101cf2be62
le-yo <le-yo@leyo.com>
2022-03-25 22:20:12 +0300

    Added config file

--- config/stk.php
+++ config/stk.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
-    //
+    //shortcode
+    'shortcode' => env('STK_SHORTCODE'),
+
+    //password
+    'password' => env('STK_PASSWORD'),
+
+    //passkey
+    'passKey' => env('STK_PASSKEY'),
+
+    //stk endpoint
+    'endpoint' => env('STK_ENDPOINT','https://sandbox.api.helaplus.com/v1/mpesa/initiateStk'),
+
+    //helaplus api key
+    'helaplus_api_key' => env('HELAPLUS_API_KEY'),
+
+
 ];
