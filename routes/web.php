10ff1cb21db68cd33f7ee682f0f6d73cb6d4d838
le-yo <le-yo@leyo.com>
2022-03-26 01:10:15 +0300

    Added stk/receiver route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Helaplus\Stk\Http\Controllers\StkController;
 
-Route::post('/stk/paymentReceiver', [StkController::class, 'paymentReceiver'])->name('payment.receiver');
+Route::post('/stk/receiver', [StkController::class, 'receiver'])->name('payment.receiver');
